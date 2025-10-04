49faa3a8f41ca3b5d8a19200b7bdc76c373f3d4e
khaled <khaledbahnawy@gmail.com>
2025-10-04 07:15:39 +0300

    create predication real cubit and integrate it

--- lib/futures/upload/presentation/view/upload_view.dart
+++ lib/futures/upload/presentation/view/upload_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nasa_app/core/di/set_up_get_it.dart';
 import 'package:nasa_app/core/widgets/coustem_drawer.dart';
+import 'package:nasa_app/futures/upload/data/repo/upload_repo_impl.dart';
+import 'package:nasa_app/futures/upload/presentation/managers/prediction_real_cubit/prediction_real_cubit.dart';
 import 'package:nasa_app/futures/upload/presentation/view/widgets/upload_view_body.dart';
 
 class UploadView extends StatelessWidget {
@@ -7,9 +11,13 @@ class UploadView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: CoustemDrawer(),
-      body: SafeArea(child: UploadViewBody()),
+    return BlocProvider(
+      create: (context) =>
+          PredictionRealCubit(getIt<UploadRepoImpl>(instanceName: 'DataRepo')),
+      child: Scaffold(
+        drawer: CoustemDrawer(),
+        body: SafeArea(child: UploadViewBody()),
+      ),
     );
   }
 }
---
+++ lib/futures/upload/presentation/view/widgets/predication_real_bloc_listener.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nasa_app/core/functions/navigate_extension.dart';
+import 'package:nasa_app/core/routes/routes.dart';
+import 'package:nasa_app/futures/auth/presentation/view/widgets/sign_up_bloc_listener.dart';
+import 'package:nasa_app/futures/upload/presentation/managers/prediction_real_cubit/prediction_real_cubit.dart';
+
+class PredicationRealBlocListener extends StatelessWidget {
+  const PredicationRealBlocListener({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<PredictionRealCubit, PredictionRealState>(
+      listener: (context, state) {
+        if (state is PredictionRealSuccess) {
+          context.pop();
+          context.pushNamed(Routes.resultView);
+        } else if (state is PredictionRealFailure) {
+          context.pop();
+          setUpErrorState(context, state.message);
+        } else {
+          setUpLoadingState(context);
+        }
+      },
+      child: SizedBox.shrink(),
+    );
+  }
+}
